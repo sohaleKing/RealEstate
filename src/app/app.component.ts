6652f9ebe34bd9cd823018a8ea51c57a7eb5fafa
Soheyl Rahgozar <St-201907648@mcit.local>
2019-11-18 20:56:39 -0500

    sss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faUserCog, faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RealEstate';
+  faUserCog = faUserCog;
+  faBars = faBars;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
-import { DetailsComponent } from './details/details.component';
+import { DetailsComponent } from './details/details.component';   
 import { SearchComponent } from './search/search.component';
 import { GuidsComponent } from './guids/guids.component';
 import { ShowComponent } from './show/show.component';
 import { ContentComponent } from './content/content.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes : Routes =[
+{path : '', component: ContentComponent},
+{path : 'login', component: LoginComponent},
+{path : 'registration', component: RegistrationComponent},
+
+
+];
 
 @NgModule({
   declarations: [
@@ -24,7 +35,8 @@ import { ContentComponent } from './content/content.component';
     ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgbModule, FontAwesomeModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
